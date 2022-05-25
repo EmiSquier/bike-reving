54ffcfd2d2fd9c071d025b2bb4548356d210d533
Emiliano Marchese <e.marchese1991@gmail.com>
2022-05-24 23:22:04 -0300

    playing woth DOM

--- js.js
+++ js.js
@@ -1,61 +1,26 @@
-usuario = prompt("Ingrese su usuario (Usuario)");
-password = prompt("Ingrese su contraseña (Contraseña)");
+let informacion = document.createElement("p");
+informacion.innerHTML = "<h3>This app will alow you to log your brand and model bike for information, tecnical specs, and users opinions<h3>";
+document.body.append(informacion);
 
-while (usuario != "Usuario"){
-  alert("Ingrese de nuevo su Usuario")
-  usuario = prompt("Ingrese su usuario");
+let subtitulo = document.getElementById("subtitulo");
+subtitulo.remove();
+
+/*let motoMarca = document.getElementById("marca").value;
+let motoModelo = document.getElementById("modelo").value;
+let motoCilindrada = document.getElementById("cilindrada").value;*/
+
+function mostrarMoto (){
+  let motoMarca = document.getElementById("marca").value;
+  let motoModelo = document.getElementById("modelo").value;
+  let motoCilindrada = document.getElementById("cilindrada").value;
+  
+  let crearMoto = document.createElement("p");
+  crearMoto.innerHTML = "Su moto es una "+motoMarca+" "+motoModelo+" de "+motoCilindrada+" cc";
+  document.body.append(crearMoto);
+  crearMoto.setAttribute("id", "crearMoto")
 }
 
-while (password != "Contraseña"){
-    alert("Ingrese de nuevo su contraseña")
-    password = prompt("Ingrese su contraseña");
+function borrarMoto(){
+  let borrarMoto = document.getElementById("crearMoto");
+  borrarMoto.remove()
 }
-
-
-let vehiculo = prompt("Ingrese su tipo de vehiculo");
-
-if (vehiculo === "moto"){
-
-class moto{ 
-  constructor(marca, modelo, potencia, cilindrada){
-      this.marca = marca;
-      this.modelo = modelo;
-      this.potencia = potencia;
-      this.cilindrada = cilindrada;
-  }
-  mostrar(){
-      alert("Su moto es una "+this.marca +" "+ this.modelo + " con una potencia de "+ this.potencia+ " hp y una cilindrada de "+this.cilindrada + " cc")
-  }
-}
-
-let marca = prompt("ingrese la marca de su vehiculo");
-let modelo = prompt("ingrese su modelo");
-let potencia = prompt("ingrese su potencia");
-let cilindrada = prompt("ingrese su cilindrada");
-
-const moto1 = new moto(marca, modelo, potencia, cilindrada);
-console.log(moto1.marca);
-moto1.mostrar()}
-else{
-  alert("Su vehículo aún no está registrado, pásese a las dos ruedas ;)");
-}
-
-//Base de datos de motocicletas
-
-let marca = ['Chevrolet', 'Kawasaki', 'Yamaha', 'Suzuki'];
-  console.log(marca);
-
-//Chevrolet no hace motos por lo que debe ser eliminado del array.
-
-let marcaEliminada = marca.shift()
-  console.log(marca);
-
-//tenemos una ktm al inicio, así que debemos agregarla
-
-marca.unshift('Ktm');
-  console.log(marca)
-
-//necesitamos encontrar la posición de YAMAHA.
-
-let pos = marca.indexOf('Yamaha');
-  console.log(pos);
