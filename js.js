3b9954fbc0f7a28fdf00bcd21c9806f3baad3e2a
Emiliano Marchese <e.marchese1991@gmail.com>
2022-06-17 02:09:37 -0300

    Api integrada

--- js.js
+++ js.js
@@ -1,13 +1,3 @@
-function bienvenida(){
-let nombre = document.getElementById("nombre").value;
-let edad = document.getElementById("edad").value;
-
-edad >= 18 ? swal(nombre, "Bienvenido!") : swal(nombre, "Me gusta tu entusiasmo!!!")
-}
-
-let botonEnviar = document.getElementById("buttonEnviar");
-  botonEnviar.addEventListener("click", bienvenida)
-
 let informacion = document.createElement("p");
 informacion.innerHTML = "<h3>This app will allow you to log your brand and model bike for information, tecnical specs, and users opinions<h3>";
 document.body.append(informacion);
@@ -28,16 +18,35 @@ function mostrarMoto (){
   crearMoto.setAttribute("id", "crearMoto")
 }
 
-function borrarMoto(){
-  let borrarMoto = document.getElementById("crearMoto");
-  borrarMoto.remove()
+
+
+/*base de datos*/
+
+const BD = [
+  {id: 1, marca: 'Kawasaki', modelo: 'er6-f', cilindrada: 650},
+  {id: 2, marca: 'Kawasaki', modelo: 'er6-n', cilindrada: 650},
+  {id: 3, marca: 'Kawasaki', modelo: 'Vulcan', cilindrada: 1600},
+  {id: 4, marca: 'Honda', modelo: 'CBR', cilindrada: 250},
+  {id: 5, marca: 'Honda', modelo: 'CB 500 x', cilindrada: 500},
+  {id: 6, marca: 'KTM', modelo: 'rc 390', cilindrada: 390},
+  {id: 7, marca: 'KTM', modelo: 'Duke 390', cilindrada: 390},
+]
+
+
+
+const [a, b, c, d, e, f, g, h] = BD;
+
+function mostrarBD(){
+  
+console.log(g);
+
+console.log(a);
+
+console.log(b)
 }
 
-let botonCrear = document.getElementById("button");
-  botonCrear.addEventListener("click", mostrarMoto);
-
-let botonBorrar = document.getElementById("buttonDelete");
-  botonBorrar.onclick = () => {borrarMoto()};
+let botonBd = document.getElementById("buttonBd");
+botonBd.onclick = () => {mostrarBD()}
 
 /*consumo de api*/
 
@@ -49,7 +58,26 @@ const options = {
 	}
 };
 
-fetch('https://motorcycle-specs-database.p.rapidapi.com/article/2012/Kawasaki/Ninja%20650', options)
+let botonCrear = document.getElementById("button");
+  botonCrear.addEventListener("click", sendButton);
+
+function sendButton(){
+
+const url = 'https://motorcycle-specs-database.p.rapidapi.com/article/'+document.getElementById("year").value+'/'+document.getElementById("marca").value+'/'+document.getElementById("modelo").value
+
+fetch(url, options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
+
+}
+
+function borrarMoto(){
+  let borrarMoto = document.getElementById("crearMoto");
+  borrarMoto.remove()
+}
+
+
+
+let botonBorrar = document.getElementById("buttonDelete");
+  botonBorrar.onclick = () => {borrarMoto()};
