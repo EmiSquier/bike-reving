d39ed6bed0f52154d393e4f9b48dd885ad7fdf3b
Emiliano Marchese <e.marchese1991@gmail.com>
2022-07-05 00:58:14 -0300

    Api in screen

--- js.js
+++ js.js
@@ -1,83 +1,39 @@
-let informacion = document.createElement("p");
-informacion.innerHTML = "<h3>This app will allow you to log your brand and model bike for information, tecnical specs, and users opinions<h3>";
-document.body.append(informacion);
-informacion.setAttribute("id", "subtitulo")
-
-let subtitulo = document.getElementById("subtitulo");
-subtitulo.remove();
-
-
-function mostrarMoto (){
-  let motoMarca = document.getElementById("marca").value;
-  let motoModelo = document.getElementById("modelo").value;
-  let motoCilindrada = document.getElementById("cilindrada").value;
-  
-  let crearMoto = document.createElement("p");
-  crearMoto.innerHTML = "Su moto es una "+motoMarca+" "+motoModelo+" de "+motoCilindrada+" cc";
-  document.body.append(crearMoto);
-  crearMoto.setAttribute("id", "crearMoto")
-}
-
-
-
-/*base de datos*/
-
-const BD = [
-  {id: 1, marca: 'Kawasaki', modelo: 'er6-f', cilindrada: 650},
-  {id: 2, marca: 'Kawasaki', modelo: 'er6-n', cilindrada: 650},
-  {id: 3, marca: 'Kawasaki', modelo: 'Vulcan', cilindrada: 1600},
-  {id: 4, marca: 'Honda', modelo: 'CBR', cilindrada: 250},
-  {id: 5, marca: 'Honda', modelo: 'CB 500 x', cilindrada: 500},
-  {id: 6, marca: 'KTM', modelo: 'rc 390', cilindrada: 390},
-  {id: 7, marca: 'KTM', modelo: 'Duke 390', cilindrada: 390},
-]
-
-
-
-const [a, b, c, d, e, f, g, h] = BD;
-
-function mostrarBD(){
-  
-console.log(g);
-
-console.log(a);
-
-console.log(b)
-}
-
-let botonBd = document.getElementById("buttonBd");
-botonBd.onclick = () => {mostrarBD()}
-
-/*consumo de api*/
-
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '18b0f3c3ddmsh476e3145da49a75p17a59fjsn4e6650baf93d',
-		'X-RapidAPI-Host': 'motorcycle-specs-database.p.rapidapi.com'
-	}
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '18b0f3c3ddmsh476e3145da49a75p17a59fjsn4e6650baf93d',
+    'X-RapidAPI-Host': 'motorcycle-specs-database.p.rapidapi.com'
+  }
 };
 
-let botonCrear = document.getElementById("button");
-  botonCrear.addEventListener("click", sendButton);
+const url = 'https://motorcycle-specs-database.p.rapidapi.com/model/make-name/' + document.getElementById("marca").value
 
-function sendButton(){
+const urlSpec = 'https://motorcycle-specs-database.p.rapidapi.com/article/' + document.getElementById('year').value + '/' + document.getElementById('marca').value + '/' + document.getElementById('modelo').value
 
-const url = 'https://motorcycle-specs-database.p.rapidapi.com/article/'+document.getElementById("year").value+'/'+document.getElementById("marca").value+'/'+document.getElementById("modelo").value
+let buton = document.getElementById("button")
+buton.addEventListener('click', logMarca)
 
-fetch(url, options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+let butonModelo = document.getElementById("buttonModelo")
+butonModelo.addEventListener('click', logModelo)
 
+function logModelo() {
+
+  const arrayModelos = fetch(url, options)
+    .then(response => response.json())
+
+  console.log(arrayModelos)
 }
 
-function borrarMoto(){
-  let borrarMoto = document.getElementById("crearMoto");
-  borrarMoto.remove()
+function logMarca() {
+  const arrayMarcas = fetch('https://motorcycle-specs-database.p.rapidapi.com/make', options).then(response => response.json())
+  console.log(arrayMarcas)
 }
 
+function logSpecs() {
+  const arraySpecs = fetch(urlSpec, options)
+    .then(response => response.json())
+  console.log(arraySpecs)
+}
 
-
-let botonBorrar = document.getElementById("buttonDelete");
-  botonBorrar.onclick = () => {borrarMoto()};
+let buttonSpecs = document.getElementById("buttonSpecs");
+buttonSpecs.addEventListener('click', logTabla());
---
+++ script.js
@@ -0,0 +1,95 @@
+const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '18b0f3c3ddmsh476e3145da49a75p17a59fjsn4e6650baf93d',
+      'X-RapidAPI-Host': 'motorcycle-specs-database.p.rapidapi.com'
+    }
+  };
+
+const urlSpec = 'https://motorcycle-specs-database.p.rapidapi.com/article/' + document.getElementById('year').value + '/' + document.getElementById('marca').value + '/' + document.getElementById('modelo').value
+
+async function getData(){
+    const response = await fetch(urlSpec, options);
+    const data = await response.json();
+
+    const make = data.articleCompleteInfo.makeName;
+    const model = data.articleCompleteInfo.modelName;
+    const category = data.articleCompleteInfo.categoryName;
+    const year = data.articleCompleteInfo.yearName;
+
+    const displacement = data.engineAndTransmission.displacementName;
+    const engineDetail = data.engineAndTransmission.engineDetailName;
+    const compression = data.engineAndTransmission.compressionName;
+    const boreXStroke = data.engineAndTransmission.boreXStrokeName;
+    const fuelSystem = data.engineAndTransmission.fuelSystemName;
+    const ignition = data.engineAndTransmission.ignitionName;
+    const coolingSystem = data.engineAndTransmission.coolingSystemName;
+    const gearbox = data.engineAndTransmission.gearboxName;
+    const transmissionTypeFinalDrive = data.engineAndTransmission.transmissionTypeFinalDriveName;
+    const driveline = data.engineAndTransmission.drivelineName;
+
+    const frameType = data.chassisSuspensionBrakesAndWheels.frameTypeName;
+    const frontBrakes = data.chassisSuspensionBrakesAndWheels.frontBrakesName;
+    const frontBrakesDiameter = data.chassisSuspensionBrakesAndWheels.frontBrakesDiameterName;
+    const frontSuspension = data.chassisSuspensionBrakesAndWheels.frontSuspensionName;
+    const frontTyre = data.chassisSuspensionBrakesAndWheels.frontTyreName;
+    const rake = data.chassisSuspensionBrakesAndWheels.rakeName;
+    const rearBrakes = data.chassisSuspensionBrakesAndWheels.rearBrakesName;
+    const rearBrakesDiameter = data.chassisSuspensionBrakesAndWheels.rearBrakesDiameterName;
+    const rearSuspension = data.chassisSuspensionBrakesAndWheels.rearSuspensionName;
+    const rearTyre = data.chassisSuspensionBrakesAndWheels.rearTyreName;
+    const trail = data.chassisSuspensionBrakesAndWheels.trailName;
+
+    const fuelCapacity = data.physicalMeasuresAndCapacities.fuelCapacityName;
+    const overallHeight = data.physicalMeasuresAndCapacities.overallHeightName;
+    const overallLength = data.physicalMeasuresAndCapacities.overallLengthName;
+    const overallWidth = data.physicalMeasuresAndCapacities.overallWidthName;
+    const seatHeight = data.physicalMeasuresAndCapacities.seatHeightName;
+    const weightInclOilGasEtcN = data.physicalMeasuresAndCapacities.weightInclOilGasEtcName;
+
+    const colorOptions = data.otherSpecifications.colorOptionsName;
+    const factoryWarrantyName = data.otherSpecifications.factoryWarrantyNameName;
+    const starter = data.otherSpecifications.starterName;
+
+    document.getElementById('make').textContent = make;
+    document.getElementById('model').textContent = model;
+    document.getElementById('category').textContent = category;
+    document.getElementById('year').textContent = year;
+
+    document.getElementById('displacement').textContent = displacement;
+    document.getElementById('engineDetail').textContent = engineDetail;
+    document.getElementById('compression').textContent = compression;
+    document.getElementById('boreXStroke').textContent = boreXStroke;
+    document.getElementById('fuelSystem').textContent = fuelSystem;
+    document.getElementById('ignition').textContent = ignition;
+    document.getElementById('coolingSystem').textContent = coolingSystem;
+    document.getElementById('gearbox').textContent = gearbox;
+    document.getElementById('transmissionTypeFinalDrive').textContent = transmissionTypeFinalDrive;
+    document.getElementById('driveline').textContent = driveline;
+
+    document.getElementById('frameType').textContent = frameType;
+    document.getElementById('frontBrakes').textContent = frontBrakes;
+    document.getElementById('frontBrakesDiameter').textContent = frontBrakesDiameter;
+    document.getElementById('frontSuspension').textContent = frontSuspension;
+    document.getElementById('frontTyre').textContent = frontTyre;
+    document.getElementById('rake').textContent = rake;
+    document.getElementById('rearBrakes').textContent = rearBrakes;
+    document.getElementById('rearBrakesDiameter').textContent = rearBrakesDiameter;
+    document.getElementById('rearSuspension').textContent = rearSuspension;
+    document.getElementById('rearTyre').textContent = rearTyre;
+    document.getElementById('trail').textContent = trail;
+
+    document.getElementById('fuelCapacity').textContent = fuelCapacity;
+    document.getElementById('overallHeight').textContent = overallHeight;
+    document.getElementById('overallLength').textContent = overallLength;
+    document.getElementById('overallWidth').textContent = overallWidth;
+    document.getElementById('seatHeight').textContent = seatHeight;
+    document.getElementById('weightInclOilGasEtcN').textContent = weightInclOilGasEtcN;
+
+    document.getElementById('colorOptions').textContent = colorOptions;
+    document.getElementById('factoryWarrantyName').textContent = factoryWarrantyName;
+    document.getElementById('starter').textContent = starter;
+}
+
+let buttonSpecs = document.getElementById("buttonSpecs");
+buttonSpecs.addEventListener('click', getData);
